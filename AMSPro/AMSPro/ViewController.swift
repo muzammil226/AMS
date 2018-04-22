bb80cdc058ff70b009180ff810a1da291bd989b4
Muzammil <sshah@fusiosd.com>
2018-04-22 09:34:46 +0400

    SMS

--- AMSPro/AMSPro/ViewController.swift
+++ AMSPro/AMSPro/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	
+	/// Changes made by muzammil
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
